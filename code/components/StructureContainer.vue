8d128c5b0b911bac33dc960fa43329e9f1900680
Mehdi Valette <mehdi.valette@gmail.com>
2021-06-10 12:04:39 +0200

    feat(structure): pan and zoom the SVG
    Work in progress; have to correct the ZOOM x and y position.
    Now it doesn't zoom where the cursor is pointing.

--- code/components/StructureContainer.vue
+++ code/components/StructureContainer.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
 svg(viewBox="0 0 1000 1000").svg
-  rect(x="0" y="0" width="1000" height="1000" fill="lightGrey")
+  g(ref="panzoom")
+    g
+      rect(x="0" y="0" width="1000" height="1000" fill="lightGrey")
 
-  structure-link(:from="{x: 30, y: 30, id: 'blabla'}" :to="{x: 100, y: 100, id: 'blablabla'}")
+      structure-link(:from="{x: 30, y: 30, id: 'blabla'}" :to="{x: 100, y: 100, id: 'blablabla'}")
 
-  structure-claim(:position="{x: 30, y: 30}" :number="3" :text="three")
+      structure-claim(:position="{x: 30, y: 30}" :number="3" :text="three")
 
-  structure-claim(:position="{x: 100, y: 100}" :number="4" :text="four")
+      structure-claim(:position="{x: 100, y: 100}" :number="4" :text="four")
 </template>
 
 <script lang="ts">
 import {Component, Vue, Provide} from 'vue-property-decorator';
 import StructureClaim from '@/components/StructureClaim.vue';
 import StructureLink from '@/components/StructureLink.vue';
+import panzoom, {PanZoom} from 'panzoom';
 
 @Component({
   components: {
@@ -21,7 +24,20 @@ import StructureLink from '@/components/StructureLink.vue';
   }
 })
 export default class StructureContainer extends Vue {
-  @Provide() private scale = {val: 1};
+  @Provide()
+  private scale = {val: 1};
+
+  mounted() {
+    const panzoomElement = this.$refs.panzoom as HTMLElement;
+    const panZoomOffset = panzoomElement.getBoundingClientRect();
+
+    const instance: PanZoom = panzoom(this.$refs.panzoom as HTMLElement, {
+      bounds: true,
+      beforeWheel: (e) => {
+        // TODO: find a way to correct the cursor position
+      } 
+    });
+  }
 }
 </script>
 
